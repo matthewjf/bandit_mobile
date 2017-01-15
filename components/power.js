ddeb4fb07e0300a4eac4643c71c66de64595e773
matthew <matthewjf@gmail.com>
2017-01-15 14:04:34 -0800

    added app icon

--- components/power.js
+++ components/power.js
@@ -3,6 +3,7 @@ import { StyleSheet, View } from 'react-native';
 import Button from './button';
 import styles, { colors, vw } from '../styles/all';
 import { htpcClick, receiverClick, TV_URL, error } from './util';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class Power extends Component {
   pcClick() {
@@ -25,6 +26,10 @@ export default class Power extends Component {
   render() {
     return (
       <View style={[styles.flexRow, styles.center, style.power]}>
+        <Icon name='record'
+              size={6*vw}
+              color={colors.red}
+              style={style.pcStatus}/>
         <Button icon='desktop-tower' color={colors.cyan} onDown={this.pcClick} style={style.btn}/>
         <Button icon='television' color={colors.red} onDown={this.tvClick} style={style.btn}/>
         <Button icon='surround-sound' color={colors.orange} onDown={this.receiverClick} style={style.btn}/>
@@ -41,5 +46,12 @@ const style = StyleSheet.create({
   },
   btn: {
     margin: 0
+  },
+  pcStatus: {
+    position: 'absolute',
+    top: 3*vw,
+    left: 12*vw,
+    zIndex: 10,
+    backgroundColor: 'transparent'
   }
 });
